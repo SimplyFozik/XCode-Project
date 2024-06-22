26141933021cb7e73b9ac48290e5a1f57db21b54
Fozik <fozikwashere@mail.ru>
2024-06-22 12:46:03 +0500

    Added ability to send messages from python to c++ + optimizations

--- Python/Terminal.py
+++ Python/Terminal.py
@@ -2,6 +2,7 @@ import socket
 import Encrypt
 import Decrypt
 import threading
+import time
 
 socketServer = socket.socket()
 serverHost = socket.gethostname()
@@ -10,33 +11,50 @@ localIP = '127.0.0.1'
 
 socketServer.connect((localIP, port))
 
+clientName = "Python"
 
 def funcSendMessages():
-    while (True):
-        print("hello")
-    
+    while True:
+        msg = input("Enter message to send: ")
+        msg_size = len(msg)
+        msg_size_bytes = msg_size.to_bytes(4,"little")
+        print(msg_size_bytes)
+        seed = Encrypt.funcGenerateSeed()
+        str_seed = str(seed)
+        str_seed = str_seed[1:]
+        str_seed = str_seed.replace(" ",'')
+        str_seed = str_seed[0:145]
+        
+        enc_text = Encrypt.funcEncrypt(seed, msg)
+
+        socketServer.send(msg_size_bytes)
+        socketServer.send(enc_text.encode())
+        socketServer.send(str_seed.encode())
+
+
 
 def funcWaitForAnswer():
     while True:
-        msg_size_bytes = socketServer.recv(4)  # Receive message size (4 bytes)
-        msg_size = int.from_bytes(msg_size_bytes, byteorder='big')  # Convert bytes to integer
-        msg = socketServer.recv(msg_size).decode()  # Receive the message based on msg_size
-        seed = socketServer.recv(145).decode()  # Receive the seed
+        msg_size_bytes = socketServer.recv(4)
+        print(msg_size_bytes)
+        msg_size = int.from_bytes(msg_size_bytes, byteorder='big')
+        msg = socketServer.recv(msg_size)
+        seed = socketServer.recv(145).decode() 
 
-        dec_msg = Decrypt.funcDecrypt(seed,msg)
+        dec_msg = Decrypt.funcDecrypt(seed, msg.decode())
 
-        print(f"Received message: {msg}")
+        print(f"Received message: {msg.decode()}")
         print(f"Received seed: {seed}")
         print(f"Decrypted Message: {dec_msg}")
 
 
 thread1 = threading.Thread(target=funcWaitForAnswer, name="Thread1")
-#thread2 = threading.Thread(target=funcSendMessages, name="Thread2")
+thread2 = threading.Thread(target=funcSendMessages, name="Thread2")
 
 
 thread1.start()
-#thread2.start()
+thread2.start()
 
 
 thread1.join()
-#thread2.join()
+thread2.join()
